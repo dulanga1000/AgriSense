b81772cd4cfbdc7485f067dbfe08041eb8567e7c
dulanga100 <dulanga1000@gmail.com>
2026-02-19 13:42:56 +0530

    Create login

---
+++ agrisense/lib/login.dart
@@ -0,0 +1,8 @@
+import 'package:';flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}

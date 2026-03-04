98e8d9348c953811bcdd5432c9e6f0c65b15d0d0
dulanga100 <dulanga1000@gmail.com>
2026-03-04 11:26:33 +0530

    Implement bottom navigation architecture with MainScreen controller and reusable BottomNavBarWidget

--- agrisense/lib/presentation/screens/home/home_screen.dart
+++ agrisense/lib/presentation/screens/home/home_screen.dart
@@ -1,34 +1,13 @@
 import 'package:flutter/material.dart';
-import '../../widgets/bottom_nav_bar.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-  final List<Widget> _screens = [
-    Center(child: Text("Home Screen")),
-    Center(child: Text("Scan Screen")),
-    Center(child: Text("Weather Screen")),
-    Center(child: Text("Profile Screen")),
-  ];
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(index: _currentIndex, children: _screens),
-      bottomNavigationBar: BottomNavBarWidget(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
+      appBar: AppBar(title: const Text('Home')),
+      body: const Center(child: Text('Home Screen')),
     );
   }
 }

35c577be7a53489f17ae97b2975b5042e77d1896
dulanga100 <dulanga1000@gmail.com>
2026-03-09 11:57:30 +0530

    feat: add FeatureGrid and FeatureCard widgets with navigation to fertilizer and crop advisory screens

---
+++ agrisense/lib/presentation/home/widgets/feature_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:agrisense/data/models/feature_model.dart';
+
+class FeatureCard extends StatelessWidget {
+  final FeatureModel feature;
+  final VoidCallback onTap;
+
+  const FeatureCard({super.key, required this.feature, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.25),
+              blurRadius: 6,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              feature.image,
+              width: 50,
+              height: 50,
+              fit: BoxFit.contain,
+            ),
+
+            const SizedBox(height: 10),
+
+            Text(
+              feature.name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 6),
+
+            Expanded(
+              child: Text(
+                feature.description,
+                textAlign: TextAlign.center,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 13, color: Colors.grey),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

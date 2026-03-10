2717394f93c2634153659e7394a2f4c6b3744c2f
KTharushi11 <tharushinimsara702@gmail.com>
2026-03-10 17:37:50 +0530

    Update auth_button.dart

--- agrisense/lib/presentation/auth/widgets/auth_button.dart
+++ agrisense/lib/presentation/auth/widgets/auth_button.dart
@@ -19,32 +19,32 @@ class AuthButton extends StatelessWidget {
     return SizedBox(
       width: double.infinity,
       child: button.outlined
-          ? OutlinedButton.icon(
-              onPressed: onPressed,
-              icon: iconWidget ?? const SizedBox(),
-              label: Text(button.text),
-              style: OutlinedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 14),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            )
-          : ElevatedButton.icon(
-              onPressed: onPressed,
-              icon: iconWidget ?? const SizedBox(),
-              label: Text(
-                button.text,
-                style: const TextStyle(color: Colors.white),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF0E8F3E),
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-            ),
+    ? OutlinedButton.icon(
+        onPressed: onPressed,
+        icon: iconWidget ?? const SizedBox(),
+        label: Text(button.text),
+        style: OutlinedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 14),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      )
+    : ElevatedButton.icon(
+        onPressed: onPressed,
+        icon: iconWidget ?? const SizedBox(),
+        label: Text(
+          button.text,
+          style: const TextStyle(color: Colors.white),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF0E8F3E),
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
     );
   }
 }

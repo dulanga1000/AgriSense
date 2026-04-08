baeef7df2cf77ecb84e71258620043df95898776
dulanga100 <dulanga1000@gmail.com>
2026-04-08 23:15:31 +0530

    host error fix

--- agrisense-otp-backend/src/config/mail.ts
+++ agrisense-otp-backend/src/config/mail.ts
@@ -1,18 +1,9 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail", 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  lookup: (hostname: string, options: any, callback: any) => {
-    dns.lookup(hostname, { family: 4 }, callback);
-  },
-} as any); // ✅ THIS FIXES RED ERROR
+});
